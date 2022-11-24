7a44aca28a3ddd9e912cfafa2daa7f56b78b630e
lwqamm <72539627+lwqamm@users.noreply.github.com>
2022-11-24 21:11:52 +0800

    Add files via upload

---
+++ Musicplay/resource.h
@@ -0,0 +1,42 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Musicplay.rc 使用
+//
+#define IDD_MUSICPLAY_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDR_EXE                         130
+#define IDR_DLL                         131
+#define IDR_JPEG1                       133
+#define IDR_MENU1                       134
+#define IDR_MENU2                       135
+#define IDR_MENU3                       136
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_LIST1                       1003
+#define IDC_LIST2                       1004
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_32783                        32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_COMMAND_VALUE         32786
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
